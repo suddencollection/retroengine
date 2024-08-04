deb53d36e7c9c8ad92048572162625c45b28324f
--- <175340361+suddencollection@users.noreply.github.com>
2024-08-04 09:44:10 -0300

    pixel buffer

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,50 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-int main()
+void run()
 {
-  std::cout << "Hello world\n";
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+  sf::RenderWindow window(sf::VideoMode(800, 600), "Raycaster");
+  sf::Vector2u windowSize = window.getSize();
 
+  // Pixel Buffer
+  sf::Image image;
+  image.create(windowSize.x, windowSize.y);
+
+  // Texture
+  sf::Texture texture;
+  texture.create(windowSize.x, windowSize.y);
+
+  // Sprite
+  sf::Sprite sprite{texture};
+
+  // Rendering
   while(window.isOpen()) {
+    // Events
     sf::Event event;
     while(window.pollEvent(event)) {
       if(event.type == sf::Event::Closed)
         window.close();
     }
 
-    window.clear();
-    window.draw(shape);
+    // Single Pixel Write
+    for(unsigned x = 0; x < windowSize.x; ++x) {
+      for(unsigned y = 0; y < windowSize.y; ++y) {
+        image.setPixel(x, y, sf::Color{255, 255, 255, 255});
+      }
+    }
+
+    // Texture update
+    texture.update(image);
+
+    // Drawing
+    window.clear(); // may not be neededd, since the entire screen is overwritten
+    window.draw(sprite);
     window.display();
   }
+}
 
+int main()
+{
+  run();
   return 0;
 }
